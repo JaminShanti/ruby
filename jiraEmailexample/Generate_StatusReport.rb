c6071996121b7f309f0fcb0672b6d305a7b1ee16
Jamin Shanti <jamin_shanti@belk.com>
2016-05-31 00:02:16 -0400

    new updates

--- jiraEmailexample/Generate_StatusReport.rb
+++ jiraEmailexample/Generate_StatusReport.rb
@@ -5,8 +5,8 @@ require 'rest-client'
 require 'mail'
 
 # source in secrets
-File.readlines("/Users/jaminshanti/.password/passwordfile").each do |line|
-  key, value  = line.split("=")
+File.readlines("#{ENV["HOME"]}/.password/statusfile").each do |line|
+  key, value  = line.split('=',2)
   ENV[key] = value.strip
 end
 
@@ -16,6 +16,9 @@ end
 # ENV['jira_name']
 # ENV['jira_password']
 # ENV['manager']
+# ENV['jira_site']
+# ENV['systemdashboardURL']
+# ENV['extraLine']
 # every 5 min cron job
 # LANG="en_GB.UTF-8"
 # 0 15 * * 4 /usr/bin/ruby /Users/jaminshanti/RubymineProjects/statusReport/Generate_StatusReport.rb
@@ -32,6 +35,23 @@ end
 
 # last Sunday
 lastsunday = (Date.today - ((Date.today.wday - 0) % 7)).strftime("%A, %b %d %Y")
+# email address
+emailAddress = ENV['email_name']
+# display Name
+displayName = ENV['displayName']
+# job Title
+jobTitle = ENV['jobTitle']
+# cell Number
+cellNumber = ENV['cellNumber']
+# Work Number
+workNumber = ENV['workNumber']
+#extra Line
+if ENV['extraLine'].to_s == ''
+  extraLine="<a href=\"\">none</a>"
+else
+  extraLine = ENV['extraLine']
+end
+
 
 To_Do = ['Open','ReOpened','Approved','Defined','Design Complete']
 In_Progress = ['In_Progress','Dev Active','Dev Complete']
@@ -50,8 +70,9 @@ def statuscolumn(status)
   return columnname
 end
 
+encoded_url_dashboard = URI.encode("https://#{ENV['jira_name']}:#{ENV['jira_password']}@#{ENV['jira_site']}/#{ENV['systemdashboardURL']}")
 
-systemdashboard = JSON.parse(RestClient.get "https://#{ENV['jira_name']}:#{ENV['jira_password']}@belkdev.atlassian.net/rest/greenhopper/1.0/xboard/work/allData.json?rapidViewId=142&selectedProjectKey=TID&activeQuickFilters=637", {:accept => :json})
+systemdashboard = JSON.parse(RestClient.get encoded_url_dashboard ,{:accept => :json})
 
 workitems = systemdashboard['issuesData']['issues']
 
@@ -59,11 +80,13 @@ worklist = []
 
 workitems.each do |item|
    subtasks = []
-   worklist.push("<a href=https://belkdev.atlassian.net/browse/#{item['key']}>#{item['key']}</a>" + '&emsp;' + statuscolumn(item['status']['name'].to_s)  + '&emsp;' + item['summary'])
-   remotejson = JSON.parse(RestClient.get "https://#{ENV['jira_name']}:#{ENV['jira_password']}@belkdev.atlassian.net/rest/api/2/search?jql=issue=#{item['key']}", {:accept => :json})
+   encoded_url_issue = URI.encode("https://#{ENV['jira_name']}:#{ENV['jira_password']}@#{ENV['jira_site']}/rest/api/2/search?jql=issue=#{item['key']}")
+   worklist.push("<a href=https://#{ENV['jira_site']}/browse/#{item['key']}>#{item['key']}</a>" + '&emsp;' + statuscolumn(item['status']['name'].to_s)  + '&emsp;' + item['summary'] + '&emsp;' + item['extraFields'][1]['html'])
+   worklist.push( "<font color=\"red\">Blocked</font>") if item["flagged"]
+   remotejson = JSON.parse(RestClient.get encoded_url_issue , {:accept => :json})
    subtasks = remotejson['issues'][0]['fields']['subtasks']
    subtasks.each do |subitem|
-   worklist.push('&emsp;' + 'Open SubTask:' + '&emsp;' + "<a href=https://belkdev.atlassian.net/browse/#{subitem['key']}>#{subitem['key']}</a>" + '&emsp;'+ subitem['fields']['summary'] ) if subitem['fields']['status']['name'] != 'Closed'
+   worklist.push('&emsp;' + 'SubTask:' + '  ' + "<a href=https://#{ENV['jira_site']}/browse/#{subitem['key']}>#{subitem['key']}</a>" + '&emsp;'+ subitem['fields']['status']['name'] + '&emsp;' + subitem['fields']['summary'] ) if not ['Closed','Resolved',"Open"].include?(subitem['fields']['status']['name'])
    end
 end
 
