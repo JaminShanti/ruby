380bc75e99005bef733e13fbf7dd48b6a6762f78
JaminShanti <Jamin@tvpc.test.com>
2016-12-11 14:15:30 -0500

    folder update

--- Scan_Site/bin/Scan_Church_sites.sh
+++ Scan_Site/bin/Scan_Church_sites.sh
@@ -2,8 +2,5 @@
 
 
 
-keyword="rehearsal"
-grepweekdays="-i -e monday -e tuesday -e wednesday -e thursday -e friday -esaturday -e sunday"
-
 churchSite="http://www.churches-of-christ.net/usa/nc_churches.html"
 churchAddress=$(wget -q -O -  ${churchSite}  | grep  -E '^.*[0-9]{5}-[0-9]{4}' | sed -E 's/([0-9]{5}-[0-9]{4}).*/\1/' | sed 's/&nbsp;//')
